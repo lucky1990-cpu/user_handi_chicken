9873d60dbd38223870b0d7d876747a6444431aab
Kumar <lucky.a.kumar@accenture.com>
2021-11-17 16:34:02 +0530

    next

--- public/js/FoodItems.js
+++ public/js/FoodItems.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+    
     const ProdUrl = 'http://handi-chichen.herokuapp.com/FoodItems';
     const DevUrl = 'http://localhost:3000/FoodItems';
-    $.get(DevUrl,(result)=>{
+    $.get(ProdUrl,(result)=>{
         document.querySelector('.loader').style.visibility='hidden';
         var maindiv = document.getElementById('CardContainer');
         if(result.length>0){
