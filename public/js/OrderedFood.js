0fb6a6cf331cfc55647173d928a9d489114f331f
Kumar <lucky.a.kumar@accenture.com>
2021-11-20 13:03:14 +0530

    next

--- public/js/OrderedFood.js
+++ public/js/OrderedFood.js
@@ -74,6 +74,11 @@ const FoodListBinding = (data)=>{
     const userPhone = document.createElement('h6')
     userPhone.innerHTML=element.PhoneNo;
     userDetailDiv.appendChild(userPhone)
+    
+     const userDateTime = document.createElement('p')
+     userDateTime.innerHTML=element.DateTime;
+     userDetailDiv.appendChild(userDateTime)
+
     const userAddress= document.createElement('h6');
     userAddress.innerHTML=element.Address
     userDetailDiv.appendChild(userAddress)
