19f4d053415f368cb999410abc515bb38eea99ed
Kumar <lucky.a.kumar@accenture.com>
2021-11-24 12:48:18 +0530

    add

--- public/js/FoodItems.js
+++ public/js/FoodItems.js
@@ -1,4 +1,4 @@
-document.querySelector('#BackButton').addEventListener('click',(e)=>{
+document.querySelector('#OnNavBackButton').addEventListener('click',(e)=>{
     e.preventDefault();
     window.location.href='/ShopPage' 
   })
